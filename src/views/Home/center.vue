4789e0560fc7bec29030635bdaf4ce5990bdd99e
LHHdiv <1067546112@qq.com>
2019-08-22 10:01:46 +0800

    '登录页面静态完成'

--- src/views/Home/center.vue
+++ src/views/Home/center.vue
@@ -1,20 +1,11 @@
 <template>
   <div class="page-home-center">
-    <!-- <div class="avatar" v-if="userInfo">
-      <div class="avator-icon">
-        <img :src="userInfo.avatar" />
-        <input type="file" @change="handleUpdAvatar" />
-      </div>
-      <div class="nick-name">{{ userInfo.nickname }}</div>
-    </div> -->
-
     <div class="avatar">
       <div class="avator-icon">
-        <img src="../../assets/img/avatar.png" />
+        <img src="https://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png" />
       </div>
       <router-link to="/login" tag="div" class="nick-name">立即登录</router-link>
     </div>
-
     <ul class="my-order-tab">
       <li>
         <i class="iconfont icon-youhui"></i>
@@ -22,118 +13,223 @@
       </li>
       <li>
         <i class="iconfont icon-linggan"></i>
-        <div class="common-p">我的订单</div>
+        <div class="common-p">礼品卡</div>
       </li>
     </ul>
-
-    <router-link tag="div" to="/card" class="margin-set my-card">
-      <span class="label">卖座卡</span>
-    </router-link>
-    <div class="margin-set my-balance">
-      <span class="label">余额</span>
-    </div>
-    <div class="margin-set system-set">
-      <span class="label">退出登录</span>
-    </div>
-  </div>
+    <div class="login-nav"></div>
+      <div class="login-con">
+        <ul class="login-ul">
+          <li>
+            <a href="#">
+              <i class="iconfont icondingdan"></i>
+              <span>我的订单</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="iconfont iconrili"></i>
+              <span>周六一起拼</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="iconfont iconshouhou"></i>
+              <span>售后服务</span>
+            </a>
+            </li>
+            <li>
+            <a href="#">
+              <i class="iconfont iconyaoqing"></i>
+              <span>邀请返利</span>
+            </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="iconfont iconyouxiangou"></i>
+                  <span>优先购</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="iconfont iconjifen"></i>
+                  <span>积分中心</span>
+              </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="iconfont iconzuanshi"></i>
+                  <span>会员俱乐部</span>
+              </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="iconfont icondizhi"></i>
+                  <span>地址管理</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="iconfont iconanquan"></i>
+                  <span>账号安全</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="iconfont iconkefu"></i>
+                  <span>帮助与客服</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="iconfont iconyijianfankui1"></i>
+                  <span>意见反馈</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="login-nav"></div>
+          <div class="margin-set system-set">
+            <a href="#">
+               <span class="label">退出登录</span>
+            </a>
+          </div>
+          <div class="login-nav"></div>
+          <wy-tabbar />
+        </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
+
 export default {
   name: "center"
 };
 </script>
-
 <style lang="scss">
-.page-home-center {
-  .avatar {
-    text-align: center;
-    margin-top: -44px;
-    height: 200px;
-    padding-left: 22px;
-    padding-top: 64px;
-    background: url(https://assets.maizuo.com/h5/v5/public/app/img/bg.6837f67.png);
-    background-size: cover;
-    display: flex;
-    align-items: center;
-    color: #fff;
-    .avator-icon {
-      position: relative;
-      width: 63px;
-      height: 63px;
-      border-radius: 35px;
-      margin-right: 20px;
-      border: 2px solid #fff;
-      img {
-        display: block;
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-      }
-      input {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        opacity: 0;
-      }
-    }
-    .nick-name {
-      font-size: 16px;
-    }
-  }
-  .my-order-tab {
-    width: 100%;
-    margin: 0 auto;
-    display: flex;
-    text-align: center;
-    font-size: 13px;
-    padding: 0;
-    height: 79px;
-    background: #fff;
-    margin-bottom: 10px;
-    align-items: center;
-    li {
-      position: relative;
-      flex: 1;
-      list-style: none;
-      .iconfont {
-        font-size: 26px;
-      }
-      .common-p {
-        margin-top: 10px;
-        color: #797d82;
-      }
-    }
-  }
-  .margin-set {
-    position: relative;
-    background: #fff;
-    padding: 0 15px;
-    height: 49px;
-    display: flex;
-    align-items: center;
-    .label {
-      font-size: 16px;
-      margin-left: 15px;
-      color: #191a1b;
-      flex: 1;
-    }
-    &::after {
-      content: " ";
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      height: 1px;
-      border-bottom: 1px solid #ededed;
-      color: #ededed;
-      transform-origin: 0 100%;
-      transform: scaleY(0.5);
-      left: 13px;
-    }
-  }
-}
-</style>
+        .page-home-center .avatar {
+          text-align: center;
+          /* margin-top: -64px; */
+          height: 135px;
+          padding-left: 22px;
+          /* padding-top: 24px; */
+          background: url(https://yanxuan.nosdn.127.net/d069279e5834bbca17065a9855a014bf.png);
+          background-size: cover;
+          display: flex;
+          align-items: center;
+          color: #fff;
+        }
+        .page-home-center .avatar .avator-icon {
+            position: relative;
+            width: 63px;
+            height: 63px;
+            border-radius: 35px;
+            margin-right: 20px;
+            border: 2px solid #fff;
+        }
+        .page-home-center .avatar .avator-icon img {
+            display: block;
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+        }
+        .page-home-center .avatar .avator-icon input {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            opacity: 0;
+        }
+        .page-home-center .avatar .nick-name {
+            font-size: 16px;
+        }
+        .my-order-tab {
+            width: 100%;
+            margin: 0 auto;
+            display: flex;
+            text-align: center;
+            font-size: 13px;
+            padding: 0;
+            height: 79px;
+            background: #fff;
+            align-items: center;
+            border-bottom: 1px solid #ccc;
+        }
+        .my-order-tab li {
+            position: relative;
+            flex: 1;
+            list-style: none;
+        }
+        .my-order-tab .iconfont {
+            font-size: 26px;
+            }
+        .my-order-tab .common-p {
+            margin-top: 10px;
+            color: #797d82;
+            }
+        .margin-set {
+            position: relative;
+            background: #fff;
+            padding: 0 15px;
+            height: 49px;
+            display: flex;
+            align-items: center;
+        }
+        .margin-set a {
+            font-size: 16px;
+            margin-left: 15px;
+            color: #191a1b;
+            flex: 1;
+            text-align: center;
+            cursor: pointer;
+          }
+        .margin-set ::after {
+            content: " ";
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            height: 1px;
+            border-bottom: 1px solid #ededed;
+            color: #ededed;
+            transform-origin: 0 100%;
+            transform: scaleY(0.5);
+            left: 13px;
+          }
+
+          .login-nav{
+              height: 5px;
+              background: #ccc;
+          }
+
+          .login-con{
+              width: 375px;
+              margin: 0 auto;
+              display: flex;
+            flex-direction: column;
+            border-top: 1px solid #ccc;
+          }
+          .login-con .login-ul li{
+            width: 33.3333%;
+            float: left;
+            height: 90px;
+            text-align: center;
+            border-right: 1px solid #ccc;
+            border-bottom: 1px solid #ccc;
+            box-sizing: border-box;
+            padding-top: 22px;
+          }
+          /* .login-con .login-ul li:nth-child(3){
+              border-right: none;
+          } */
+          .login-con .login-ul li a .iconfont{
+            color: #bbb;
+            font-size: 26px;
+          }
+          .login-con .login-ul li a span{
+              display: block;
+              margin-top: 3px;
+          }
+
+
+      </style>
